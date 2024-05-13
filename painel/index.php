e2582f3b717efa2e25976c1bb1f619435591ea94
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-14 02:59:44 +0300

    Atualizações

--- painel/index.php
+++ painel/index.php
@@ -14,12 +14,136 @@
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <?php
-        $query = "select * from mohatron_painel.lib where versao = 'v1'";
-        $result = mysql_query($query);
-        $d_lib = mysql_fetch_object($result);
-        echo $d_lib->lib;
-    ?>
+		<!-- lib -->
+
+        <link href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" rel="stylesheet" type="text/css" >
+
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css">
+        <link href="http://mohatron.com.br/lib/v1/fonts_grafic/fonte.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/jquery.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/bootstrap/bootstrap.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/search/jquery.quicksearch.js"></script>
+
+<link href="https://www.mohatron.com.br/lib/v1/css/bootstrap.css" rel="stylesheet" type="text/css">
+
+<link href="https://www.mohatron.com.br/lib/v1/css/popup.css" rel="stylesheet prefetch" type="text/css">
+
+<link href="https://www.mohatron.com.br/lib/v1/css/jquery-confirm.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/jquery-confirm.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/mask/mask.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/validate/jquery.validate.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/mask/jquery.maskMoney.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/jquery-ui.js"></script>
+
+<link href="https://www.mohatron.com.br/lib/v1/css/jquery-ui.css" rel="stylesheet" type="text/css">
+
+<link href="https://www.mohatron.com.br/lib/v1/css/easy-responsive-tabs.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/easy-responsive-tabs.js"></script>
+
+<link href="https://www.mohatron.com.br/lib/v1/css/templatemo-misc.css" rel="stylesheet" type="text/css">
+
+       <!-- <link href="https://www.mohatron.com.br/lib/v1/fonts/fonte.css" rel="stylesheet" type="text/css"> -->
+
+<link href="https://www.mohatron.com.br/lib/v1/datapicker/css.css" rel="stylesheet">
+
+<script src="https://www.mohatron.com.br/lib/v1/datapicker/locales.js"></script>
+
+<script src="https://www.mohatron.com.br/lib/v1/datapicker/js.js"></script>
+
+<script src="https://www.mohatron.com.br/lib/v1/datapicker/jquery-ui.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://www.mohatron.com.br/lib/v1/datapicker/jquery-ui.css">
+
+<link rel="stylesheet" type="text/css" href="https://www.mohatron.com.br/lib/v1/paginacao/jqpagination.css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/paginacao/jquery.jqpagination.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/bootstrap/bootstrap-filestyle.js"></script>
+
+   <link href="https://www.mohatron.com.br/lib/v1/css/select.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/bootstrap/select.js"></script>
+
+<link href="https://www.mohatron.com.br/lib/v1/bootstrap-toggle/bootstrap-toggle.css" rel="stylesheet">
+
+<script src="https://www.mohatron.com.br/lib/v1/bootstrap-toggle/bootstrap-toggle.js"></script>
+
+   <link href="https://www.mohatron.com.br/lib/v1/maxlength/css.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/maxlength/js.js"></script>
+
+<link href="https://www.mohatron.com.br/lib/v1/fileinput/css.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/fileinput/js.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/fileinput/jq.js"></script>
+
+<script src="https://www.mohatron.com.br/lib/v1/graficos/highstock.js"></script>
+
+<script src="https://www.mohatron.com.br/lib/v1/graficos/exporting.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/ckeditor/ckeditor.js"></script>
+
+
+
+
+<script src="https://www.mohatron.com.br/lib/v1/sms/chart/dist/Chart.bundle.js"></script>
+
+<script src="https://www.mohatron.com.br/lib/v1/sms/chart/samples/utils.js"></script>
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.9/css/bootstrap-dialog.min.css" rel="stylesheet" type="text/css" />
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.9/js/bootstrap-dialog.min.js"></script>	
+
+<!--<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/dropdowns.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/price-range.js"></script>
+
+<link href="https://www.mohatron.com.br/lib/v1/css/price-range.css" rel="stylesheet" type="text/css">
+
+<link href="https://www.mohatron.com.br/lib/v1/css/dropdowns-skin-discrete.css" rel="stylesheet" type="text/css">
+
+<link href="https://www.mohatron.com.br/lib/v1/css/dropdowns-skin-default.css" rel="stylesheet" type="text/css">
+
+<link href="https://www.mohatron.com.br/lib/v1/css/dropdowns.css" rel="stylesheet" type="text/css">-->
+
+
+<link href="https://www.mohatron.com.br/lib/v1/crop/croppic.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/crop/croppic.min.js"></script>
+
+
+<link href="https://www.mohatron.com.br/lib/v1/slick/slick.css" rel="stylesheet" type="text/css">
+
+<link href="https://www.mohatron.com.br/lib/v1/slick/slick-theme.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/slick/slick.min.js"></script>
+
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/fullscreen/jquery.fullscreen.min.js"></script>
+
+
+
+<link href="https://www.mohatron.com.br/lib/v1/scrollbar/scrollbar.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/scrollbar/scrollbar.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/contador/waypoints.min.js"></script>
+
+<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/contador/jquery.counterup.min.js"></script>
+
+
+
+<!-- fim lib -->		
     <style>
         .Cpg{
             position:fixed;
@@ -46,11 +170,26 @@
         include("includes/login.php");
     }
 
-
-    echo $d_lib->lib_rodape;
-
 ?>
     
+
+    		<!-- lib -->
+		
+		<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/jquery.easing-1.3.js"></script>
+		
+		<!--<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/plugins.js"></script>-->
+		
+		<!--<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/main.js"></script>-->
+		
+		<!--<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/acoes.js"></script>-->
+		
+		<!--<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/popup.js"></script>-->
+		
+		<!--<script type="text/javascript" src="https://www.mohatron.com.br/lib/v1/jquery/popup-responsive.js"></script>-->
+		
+		<!-- fim lib -->
+
+
     <script>
         
         $(function(){
